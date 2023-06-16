6059defef1fd006effe2d996b4ead2839781a241
Romitou <romitou@protonmail.com>
2023-06-16 15:24:48 +0200

    Listen to packets with gopacket

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/romitou/tcpmirror
 
 go 1.19
+
+require github.com/google/gopacket v1.1.19 // indirect
--- main.go
+++ main.go
@@ -2,40 +2,37 @@ package main
 
 import (
 	"flag"
-	"io"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
 	"log"
 	"net"
 )
 
 func main() {
+	interfaceName := flag.String("i", "eth0", "the interface to get packets from")
 	inPort := flag.String("in", ":25565", "the port to listen on")
 	outPort := flag.String("out", ":25566", "the port to mirror to")
 	flag.Parse()
 
-	listeningPort, err := net.Listen("tcp", *inPort)
+	handle, err := pcap.OpenLive(*interfaceName, 65536, true, pcap.BlockForever)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer func(listeningPort net.Listener) {
-		err = listeningPort.Close()
+	if err = handle.SetBPFFilter("tcp and port " + *inPort); err != nil {
+		log.Fatal(err)
+	}
+
+	for packet := range gopacket.NewPacketSource(handle, handle.LinkType()).Packets() {
+		var mirrorConn net.Conn
+		mirrorConn, err = net.Dial("tcp", *outPort)
 		if err != nil {
 			log.Fatal(err)
 		}
-	}(listeningPort)
 
-	mirrorConn, err := net.Dial("tcp", *outPort)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for {
-		incomingConn, _ := listeningPort.Accept()
-		go func() {
-			_, err = io.Copy(incomingConn, mirrorConn)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}()
+		_, err = mirrorConn.Write(packet.Data())
+		if err != nil {
+			log.Println(err)
+		}
 	}
 }
